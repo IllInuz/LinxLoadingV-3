7a18dfba23699806aa867ce0f3b38b418035fb5f
Illinuz <snellemany@gmail.com>
2024-11-18 13:46:51 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+description 'LinxLoading V3'
+author 'Lorenc'
+version '3.0.0'
+
+client_scripts {
+    "client.lua",
+}
+
+files {
+    'html/*.*',
+}
+
+loadscreen_manual_shutdown "yes"
+loadscreen 'html/index.html'
+lua54 'yes'
